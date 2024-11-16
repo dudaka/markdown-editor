cb03ae96cd31f1efd421ae5a99d874aa1c36ea06
Dung Ho <htdung820@gmail.com>
2024-11-16 01:48:08 -0700

    Adding drag and drop support

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
+var editor;
+
 window.addEventListener('DOMContentLoaded', () => {
-  const editor = new SimpleMDE({ element: document.getElementById("editor") });
+  editor = new SimpleMDE({ element: document.getElementById("editor") });
 
   window.api.editorEventRegister((arg) => {
     if (arg === 'toggle-bold') {
@@ -15,3 +17,29 @@ window.addEventListener('DOMContentLoaded', () => {
     editor.value(content);
   });
 });
+
+
+function dropHandler(event) {
+  console.log('File(s) dropped');
+  
+  event.preventDefault();
+  
+  if (event.dataTransfer.items) {
+    for (let i = 0; i < event.dataTransfer.items.length; i++) {
+      if (event.dataTransfer.items[i].kind === 'file') {
+        const file = event.dataTransfer.items[i].getAsFile();
+        if (file.type === 'text/markdown') {
+          const reader = new FileReader();
+          reader.onload = (event) => {
+            const content = event.target.result;
+            // window.api.sendEventToEditor('load', content);
+            // console.log(event.target.result);
+            editor.codemirror.setValue(content);
+            
+          };
+          reader.readAsText(file);
+        }
+      }
+    }
+  }
+}
